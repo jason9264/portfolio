8c6e17e7dc71813ead084ee8188839bfb0e4a34a
jason9264 <jason9264@outlook.com>
2024-10-16 23:31:16 -0400

    all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   Main,
   Timeline,
@@ -14,30 +14,37 @@ import './index.scss';
 function App() {
     const [mode, setMode] = useState<string>('dark');
 
+    // Load mode from local storage
+    useEffect(() => {
+        const savedMode = localStorage.getItem('mode') || 'dark'; 
+        setMode(savedMode);
+    }, []);
+
+    // Save mode to local storage
+    useEffect(() => {
+        localStorage.setItem('mode', mode);
+    }, [mode]);
+
     const handleModeChange = () => {
-        if (mode === 'dark') {
-            setMode('light');
-        } else {
-            setMode('dark');
-        }
+        setMode(prevMode => (prevMode === 'dark' ? 'light' : 'dark'));
     }
 
     useEffect(() => {
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-      }, []);
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    }, []);
 
     return (
-    <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
-        <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
-        <FadeIn transitionDuration={700}>
-            <Main/>
-            <Expertise/>
-            <Timeline/>
-            <Project/>
-            <Contact/>
-        </FadeIn>
-        <Footer />
-    </div>
+        <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
+            <Navigation parentToChild={{ mode }} modeChange={handleModeChange} />
+            <FadeIn transitionDuration={700}>
+                <Main />
+                <Expertise />
+                <Timeline />
+                <Project />
+                <Contact />
+            </FadeIn>
+            <Footer />
+        </div>
     );
 }
 
