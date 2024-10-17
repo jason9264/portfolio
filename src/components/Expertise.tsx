71d8d1f3274096d54831927ce09c1b5c895f7778
jason9264 <jason9264@outlook.com>
2024-10-17 12:19:41 -0400

    remove animations

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -48,7 +48,7 @@ function Expertise() {
             <br></br>
             <div className="skills-grid">
                 <div className="skill">
-                <FontAwesomeIcon icon={faConnectdevelop} spin size="3x" />                    
+                <FontAwesomeIcon icon={faConnectdevelop} size="3x" />                    
                 <h2>Machine Learning</h2>
                 <br></br>
                     <p>My passion for machine learning began during my Master’s in Analytics. I was captivated by its ability to solve problems beyond traditional statistics by learning and using innovative approaches. </p> <p>I really believe that machine learning will bring cutting edge advancements, and help to improve quality of life around the world.</p>
@@ -61,7 +61,7 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                <FontAwesomeIcon icon={faChartSimple} beat size="3x" />                    
+                <FontAwesomeIcon icon={faChartSimple} size="3x" />                    
                 <h2>Statistics & Analytics</h2>
                 <br></br>
                     <p>My interest in statistics was sparked by Asimov’s Foundation, where a statistician predicts the future with data.</p> <p>Analyzing large datasets gives me a unique sense of fulfillment, and every new piece of knowledge enhances my skills, allowing me to really embrace being a “jack of all trades”.</p>
@@ -74,7 +74,7 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPuzzlePiece} spin spinReverse size="3x"/>
+                    <FontAwesomeIcon icon={faPuzzlePiece} spinReverse size="3x"/>
                     <h2>Data Engineering</h2>
                     <br></br>
                     <p>I love data engineering for its big-picture perspective that merges structure and problem-solving, similar to cooking, where you consider the entire dish.</p> <p> It offers complexity and unqiue rewards by influencing all business areas, like preparing a well thought out meal.</p>
